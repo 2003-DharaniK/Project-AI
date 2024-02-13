9b2377c5fdb22915a4a78ff11c46e40207c11992
Dharani K <145276470+2003-DharaniK@users.noreply.github.com>
2024-02-13 19:21:21 +0530

    Snake water gun.py
    The updated python game

--- Snake water gun.py
+++ Snake water gun.py
@@ -1,79 +1,4 @@
 import random
-print('Winning rules of the game SNAKE WATER GUN are :\n'
-      + "Snake vs water -> Snake wins \n"
-      + "Water  vs Gun -> Water wins \n"
-      + "Gun vs Snake -> Gun wins \n")
-while True:
-    print("The choice are \n 1.Snake \n 2.Water \n 3. Gun")
-    user_choice=int(input("Enter your choice:"))
-    
-    while user_choice > 3 or user_choice < 1:
-        print(" Please Enter a valid choice:")
-
-        if user_choice == 1 :
-            user_choice_name= "Snake"
-        elif user_choice == 2:
-                user_choice_name = "Water"
-        else:
-            user_choice_name = "Gun"
-            print("The choice you picked is :",user_choice_name)
-            print(" Now It's My turn!!")
-
-            com_choice = random.randint(1,3)
-            while com_choice == user_choice:
-                com_choice = random.randint(1,3)
-
-                if com_choice == 1:
-                    com_choice_name  = "Snake"
-                elif com_choice == 2:
-                    com_choice_name = "Water"
-                else:
-                    com_choice_name = "Gun"
-                    print("My choice is :",com_choice_name)
-                    print(user_choice_name,"VS", com_choice_name)
-
-
-                    if user_choice == com_choice:
-                        print("ITS  A DRAW ",end="")
-                        result = "DRAW"
-                    if user_choice == 1 and com_choice ==2:
-                        print("SNAKE WINS",end="")
-                        result = "SNAKE"
-                    elif user_choice== 2 and com_choice == 1:
-                        print("SNAKE WINS",end="")
-                        result = "SNAKE"
-
-
-                    if user_choice == 1 and com_choice == 3:
-                        print("GUN WINS", end="")
-                        result = "GUN"
-                    elif user_choice == 3 and com_choice == 1:
-                        print("GUN WINS", end="")
-                        result = "GUN"
-
-                    if user_choice == 2 and com_choice == 3:
-                        print("WATER WINS", end="")
-                        result = "WATER"
-                    elif user_choice == 3 and com_choice == 2:
-                        print("WATER WINS",end= "")
-                        result = "WATER"
-
-                    if result == "DRAW":
-                        print("<-- Its a tie -->")
-                    if result == user_choice_name:
-                        print("<--YOU WIN-->")
-                    else:
-                        print("<-- HURRY I WIN -->")
-                    print("Do you want to play again ?(Y/N)")
-                    ans = input().lower
-                    if ans == "n":
-                        break
-                        print("THANKS FOR PLAYING WITH ME")
-'''
-
-
-
-import random
 
 print('Winning rules of the game SNAKE WATER GUN are :\n'
       + "Snake vs water -> Snake wins \n"
@@ -137,4 +62,3 @@ while True:
     if ans == "n":
         print("THANKS FOR PLAYING WITH ME")
         break
-
